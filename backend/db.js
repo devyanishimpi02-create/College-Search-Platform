67b9af52bf810670f620e4c8aa1d3ec6dfc9316e
Devayani Shimpi <devyanishimpi02@gmail.com>
2026-07-14 22:00:46 +0530

    Connect to TiDB

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,17 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -15,10 +20,10 @@ const db = mysql.createPool({
 (async () => {
   try {
     const connection = await db.getConnection();
-    console.log('Connected to MySQL database!');
+    console.log("Connected to TiDB!");
     connection.release();
   } catch (err) {
-    console.error('Database connection failed:', err);
+    console.error(err);
   }
 })();
 
