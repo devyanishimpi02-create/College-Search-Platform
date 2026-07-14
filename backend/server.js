cee8349e420be577aa184c9c5690723d8775fb6c
Devayani Shimpi <devyanishimpi02@gmail.com>
2026-07-14 19:53:55 +0530

    Fix frontend serving

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
@@ -12,12 +13,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Serve frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// API Routes
 app.use("/api", collegeRoutes);
 
 // Home Route
 app.get("/", (req, res) => {
-    res.send("College Search Platform API is running!");
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
 // Stats API
@@ -49,11 +53,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
